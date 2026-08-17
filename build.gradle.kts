c344874851f46aa20823806f81db11a1672f86bd
Sesmom <you@example.com>
2026-08-17 09:55:46 +0200

    fix: Kotlin 1.9.20 to match Compose 1.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-// Top-level build file - plugins versions are in app/build.gradle.kts
 plugins {
     id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
