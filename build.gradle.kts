b219349cd381eb914d06005b24edb0a3808e9965
Sesmom <you@example.com>
2026-08-17 09:39:48 +0200

    fix: gradle 8.4 + pluginManagement for Android build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
+// Top-level build file - plugins versions are in app/build.gradle.kts
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
